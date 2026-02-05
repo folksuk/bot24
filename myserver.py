116afb0c96aa9bddd47fa62b42804c36b0d1ebf1
Candy <116365885+folksuk@users.noreply.github.com>
2026-02-05 19:28:11 +0700

    Update myserver.py

--- myserver.py
+++ myserver.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Server is running!"
+    return "Bot is alive!"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def server_on():
     t = Thread(target=run)
